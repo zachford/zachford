2f94d4bb515bc1a1abe79958ef1daac02fe0653d
Zach Ford <zach@zachford.me>
2016-08-08 21:22:14 -0500

    Add edit and delete to website

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+
 	def create
 		@post = Post.new(post_params)
 
@@ -20,6 +21,27 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to root_path
+	end
+
+
+	def update
+		@post = Post.find(params[:id]) 
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end 
 	end
 
 	private
