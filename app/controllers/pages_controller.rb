b2b2288b963306a09b05caf95ffac0d483b85e4c
Zach Ford <zach@zachford.me>
2016-08-16 00:54:08 -0500

    Add pages and styling to respective pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,18 @@
 class PagesController < ApplicationController
+	
+	def home
+	end
+
 	def about
 	end
-	
+
+	def design
+	end
+
+	def music
+	end
+
+	def other
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,13 @@ end
 root "posts#index"
 
  get '/about', to: 'pages#about'
+
+ get '/home', to: 'pages#home'
+
+ get '/design', to: 'pages#design'
+
+ get '/music', to: 'pages#music'
+
+ get '/other', to: 'pages#other'
+ 
 end
